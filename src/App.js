3f39ba418b728885c498e63736df2ff45d15f525
alexfranco311 <alexfranco311@gmail.com>
2021-10-19 00:26:33 -0400

    Compiled Successfully! Great Success

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app';
@@ -23,12 +23,13 @@ const firestore = firebase.firestore();
 
 
 function App() {
-  const [user] = useAuthState();
+  const [user] = useAuthState(auth);
 
   return (
     <div className="App">
       <header>
-
+        <h1>CHat 89🔥</h1>
+        <SignOut />
       </header>
       <section>
         {user ? <ChatRoom /> : <SignIn />}
@@ -54,11 +55,58 @@ function SignOut() {
   )
 }
 
-Function ChatRoom() {
+function ChatRoom() {
+
+  const dummy = useRef();
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, {idField: 'id'});
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+
+    const {uid, photoURL} = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+    setFormValue('');
+
+    dummy.current.scrollIntoView({behavior: 'smooth'});
+  }
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+        <div ref={dummy}></div>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+        <button type="submit">🚀</button>
+      </form>
+      </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL} = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt={''}/>
+      <p>{text}</p>
+    </div>  
+  )
 }
 
 export default App;
